c3a274be997099eb452790c86cee202665fd4437
marcel <marcel.nicolay@gmail.com>
2011-11-21 12:07:42 -0200

    fix import name

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 # limitations under the License.
  
 from setuptools import setup, find_packages
-from simplexml import __version__
+from wffmpeg import __version__
  
 setup(
     name = 'wffmpeg',
